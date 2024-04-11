16963a858498a930d87f43e1b181acc0ef25e0a4
Chengzi-creator <1041379828@qq.com>
2024-04-11 23:47:44 +0800

    实现了局部互动从而展现新内容的效果
    但是还没有做出自如切换的那种动画

--- Assets/Scripts/Projectsettings/SmallChange.cs
+++ Assets/Scripts/Projectsettings/SmallChange.cs
@@ -8,7 +8,8 @@ public class SmallChange : MonoBehaviour
     public Material newMaterial; //需要更新的新的材质
     //public Material oldMaterial; //需要更新的旧的材质
     public GameObject parentObject;
-    
+    public bool haveClicked;//检测是否有点到
+        
     void Start()
     {
         
@@ -17,27 +18,41 @@ public class SmallChange : MonoBehaviour
 
     void Update()
     {
-        
+        if (haveClicked)
+        {       
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    
+                    //获取物体
+                    GameObject hitObject = hit.collider.gameObject;
+                    GameObject thisGameObject = gameObject;
+
+                    if (hitObject == thisGameObject)
+                    {
+                        MeshRenderer renderer = parentObject.GetComponent<MeshRenderer>();
+                        if (renderer != null)
+                            //更换物体的材质,目前还没做翻回去的效果
+                            renderer.material = newMaterial;
+                    }
+                }
+        }
     }
 
 
     void OnMouseDown()//检测鼠标左键按下
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-        RaycastHit hit;
-        
-        if (Physics.Raycast(ray, out hit))
+        if (Input.GetMouseButtonDown(0))
         {
-            //获取物体
-            GameObject hitObject = hit.collider.gameObject;
-            GameObject thisGameObject = gameObject;
-                    
-            if(hitObject == thisGameObject)
-            {      
-                MeshRenderer renderer = parentObject.GetComponent<MeshRenderer>();
-                if(renderer != null)
-                    //更换物体的材质,目前还没做翻回去的效果
-                    renderer.material = newMaterial;
+            Debug.Log("Yes");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                haveClicked = true;
             }
         }
     }
