6950b6a411f1719d0736b0fb52a9a10af66b70cd
Chengzi-creator <1041379828@qq.com>
2024-04-12 13:25:06 +0800

    试着加吸附

--- Assets/Scripts/Projectsettings/ProjectDrag.cs
+++ Assets/Scripts/Projectsettings/ProjectDrag.cs
@@ -9,11 +9,14 @@ public class ProjectDrag : MonoBehaviour
     public Vector3 offset;
     public Vector3 movePosition;
     public string targetLayer = "moveFile";
-    private int oldLayerID;   // 记录原来的层级
+    private int oldLayerID;   //记录原来的层级
+    
+    public Transform target; //手动拖入父文件的 Transform 组件
+    public float distanceHold; //吸附范围
     
     void Start()
     {
-        // 保存原来的层级，似乎修改层级
+        //保存原来的层级，似乎修改层级没有用？
         oldLayerID = gameObject.layer;
     }
     
@@ -27,6 +30,8 @@ public class ProjectDrag : MonoBehaviour
         }
     }
 
+    #region 物体拖拽，吸附和图层问题
+    
     void OnMouseDown()//鼠标按下
     {
         if (Input.GetMouseButtonDown(0))
@@ -56,7 +61,8 @@ public class ProjectDrag : MonoBehaviour
     }
 
     void OnMouseUp()//鼠标松开
-    {
+    {   
+        //————拖拽
         if (Input.GetMouseButtonUp(0))
         {
             isDragging = false;
@@ -65,9 +71,25 @@ public class ProjectDrag : MonoBehaviour
             newPosition.z += 5.0f;//鼠标松开时物体的Y轴减少一点
             transform.position = newPosition;
             
-            // 将物体的层级恢复到原来的层级
+            //将物体的层级恢复到原来的层级
             gameObject.layer = oldLayerID;
             Debug.Log("2");
         }
+        
+        //————吸附
+        //检测鼠标释放位置是否在另一父文件附近
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 targetPosition = Camera.main.WorldToScreenPoint(target.position);
+        
+        float distance = Vector3.Distance(mousePosition, targetPosition);
+
+        //如果鼠标释放位置在文件2附近，则将文件1的位置移动到文件2的位置，同时还要设置层级关系
+        if (distance < distanceHold)
+        {
+            transform.position = target.position ;
+        }
     }
+    #endregion
+
+    
 }
